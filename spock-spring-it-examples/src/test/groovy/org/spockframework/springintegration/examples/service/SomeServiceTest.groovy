46ccc62985098832d92f122798a08027f2dc05aa
Jan Machacek <jan.machacek@gmail.com>
2011-03-09 17:55:18 +0000

    Web testing ideas

--- spock-spring-it-examples/src/test/groovy/org/spockframework/springintegration/examples/service/SomeServiceTest.groovy
+++ spock-spring-it-examples/src/test/groovy/org/spockframework/springintegration/examples/service/SomeServiceTest.groovy
@@ -1,10 +1,10 @@
 package org.spockframework.springintegration.examples.service
 
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
-/**
+ /**
  * @author janm
  */
 @IntegrationTest
@@ -17,5 +17,19 @@ class SomeServiceTest extends Specification {
 		expect:
 		this.someService.idolFind().empty
 	}
+
+	/*
+	def "index"() {
+		given:
+		def message = this.hibernateTemplate.get(Message.class, 1L)
+
+		when:
+		form = get("/message/1.html")
+		response = post(form)
+
+		then:
+		this.hibernateTemplate.get(Message.class, 1L).equals(message)
+	}
+	*/
 
 }

d27bc91d1f5d799ea254b1218bec7e8749f86818
Jan Machacek <janm@cakesolutions.net>
2011-03-10 10:15:19 +0000

    Web ext

--- spock-spring-it-web/src/main/java/org/spockframework/springintegration/web/SpringWebExtension.java
+++ spock-spring-it-web/src/main/java/org/spockframework/springintegration/web/SpringWebExtension.java
@@ -1,7 +1,14 @@
 package org.spockframework.springintegration.web;
 
+import org.spockframework.runtime.extension.IGlobalExtension;
+import org.spockframework.runtime.model.SpecInfo;
+
 /**
  * @author janm
  */
-public class SpringWebExtension {
+public class SpringWebExtension implements IGlobalExtension {
+	@Override
+	public void visitSpec(SpecInfo spec) {
+		
+	}
 }

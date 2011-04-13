084f104cea7b17c5473b0fd1cf5073ad98c3f752
Jan Machacek <janm@cakesolutions.net>
2011-04-13 21:46:20 +0100

    Test-level URL class loading fixes

--- spock-spring-it-web/src/main/java/org/spockframework/springintegration/web/JspCapableMockHttpServletRequest.java
+++ spock-spring-it-web/src/main/java/org/spockframework/springintegration/web/JspCapableMockHttpServletRequest.java
@@ -4,9 +4,19 @@ import org.apache.jasper.servlet.JspServlet;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockRequestDispatcher;
 
-import javax.servlet.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * @author janm
@@ -57,15 +67,42 @@ public class JspCapableMockHttpServletRequest extends MockHttpServletRequest {
 			JspServlet s = new JspServlet();
 			try {
 				s.init(this.servletConfig);
-				s.service(new HttpServletRequestWrapper((HttpServletRequest)request) {
+				fixupClassLoading();
+				s.service(new HttpServletRequestWrapper((HttpServletRequest) request) {
 					@Override
 					public String getServletPath() {
 						return url;
 					}
 				}, response);
+				s.destroy();
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
 		}
+
+		private void fixupClassLoading() throws IOException {
+			URLClassLoader contextClassLoader = (URLClassLoader) Thread.currentThread().getContextClassLoader();
+			if (contextClassLoader instanceof DelegateURLClassLoader) return;
+
+			final Enumeration<URL> resources = contextClassLoader.findResources("META-INF");
+			List<URL> urls = new ArrayList<URL>();
+			while (resources.hasMoreElements()) {
+				URL resource = resources.nextElement();
+				final String file = resource.getFile().replace("!/META-INF", "").replace("file:", "");
+				final URL fileUrl = new File(file).toURI().toURL();
+				urls.add(fileUrl);
+			}
+			URLClassLoader delegateClassLoader = new URLClassLoader(urls.toArray(new URL[urls.size()]), contextClassLoader);
+
+			Thread.currentThread().setContextClassLoader(delegateClassLoader);
+		}
 	}
+
+	static class DelegateURLClassLoader extends URLClassLoader {
+
+		public DelegateURLClassLoader(URL[] urls, ClassLoader classLoader) {
+			super(urls, classLoader);
+		}
+	}
+
 }
